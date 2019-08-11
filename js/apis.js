ebc9d613c2d64ea0be18ee50dfdc914b32d64eb7
alearmas <aarmas17@gmail.com>
2019-08-11 12:31:36 -0300

    Inicialización de mapa

--- js/apis.js
+++ js/apis.js
@@ -1,12 +1,14 @@
 var mapa // Mapa que vamos a modificar
 
-/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
-
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
-    /* Modificá la variable mapa con el constructor Map().
-    Tendrás que asignarle un valor de zoom y
-    un centro igual a la variable posicionCentral. */
+  /* Modificá la variable mapa con el constructor Map().
+  Tendrás que asignarle un valor de zoom y
+  un centro igual a la variable posicionCentral. */
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.617134, lng:-58.445232},
+    zoom: 13
+  });
 
   geocodificadorModulo.inicializar()
   marcadorModulo.inicializar()
